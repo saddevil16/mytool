5bfbeff76273bd2182bd68b93f24f04e711d0c76
Faizal Refendi <faizal@null.net>
2026-05-14 04:28:46 +0800

    feat: add process cert function

--- .modules/jenkins_https.sh
+++ .modules/jenkins_https.sh
@@ -14,19 +14,45 @@ loadEnv() {
     set +a
 }
 
+processCert() {
+	local PASS="$SUDO_PASS"
+	local https_dir="$JENKINS_FULLCHAIN_PEM_PATH"
+	local cert_dir="$LETSENCRYPT_FULLCHAIN_PEM_PATH"
+
+	if [ ! -d "$https_dir" ]; then
+ 	    mkdir -p "$https_dir"
+    else
+		echo "Forcefully removing $https_dir"
+	 	echo "$PASS" | sudo -S rm -r "$https_dir"
+        mkdir -p "$https_dir"
+    fi
+
+    # shellcheck disable=SC2024
+    echo "$PASS" | sudo -S cat "$cert_dir/fullchain.pem" > $https_dir/fullchain.pem
+    # shellcheck disable=SC2024
+    echo "$PASS" | sudo -S cat "$cert_dir/privkey.pem" > $https_dir/privkey.pem
+    unset PASS
+
+    echo "Generated $https_dir/jenkins.jks"
+    
+    echo "Finished processing certificate."
+    read -r -p "Press any key to continue.. "
+}
+
 checkCert() {
 	local pass="$SUDO_PASS"
-	local jenkins_cert_path="$JENKINS_FULLCHAIN_PEM"
-	local certbot_jenkins_cert_path="$LETSENCRYPT_FULLCHAIN_PEM"
+	local jenkins_cert_path="$JENKINS_FULLCHAIN_PEM_PATH"
+	local certbot_jenkins_cert_path="$LETSENCRYPT_FULLCHAIN_PEM_PATH"
 
 	echo -e "${G}Jenkins Cert Expiry: ${RST}"
-	echo "Checking '$jenkins_cert_path' expiry.."
-    openssl x509 -in "$jenkins_cert_path" -noout -enddate
+	echo "Checking $jenkins_cert_path/fullchain.pem expiry.."
+    openssl x509 -in "$jenkins_cert_path/fullchain.pem" -noout -enddate
 
     echo ""
     echo -e "${G}Certbot Renewed Cert Expiry:${RST}"
-    echo "Checking $certbot_jenkins_cert_path expiry.."
-    echo "${pass}" | sudo -S openssl x509 -in "$certbot_jenkins_cert_path" -noout -enddate
+    echo "Checking $certbot_jenkins_cert_path/fullchain.pem expiry.."
+    echo "${pass}" | sudo -S openssl x509 -in "$certbot_jenkins_cert_path/fullchain.pem" -noout -enddate
+    unset pass
 
     read -r -p "Press any key to continue.."
 }
@@ -49,7 +75,11 @@ while true; do
     case $option in
         1) 
         	loadEnv && checkCert 
-        	unset SUDO_PASS JENKINS_FULLCHAIN_PEM LETSENCRYPT_FULLCHAIN_PEM
+        	unset SUDO_PASS JENKINS_FULLCHAIN_PEM_PATH LETSENCRYPT_FULLCHAIN_PEM_PATH
+        	;;
+        2)
+        	loadEnv && processCert
+        	unset SUDO_PASS JENKINS_FULLCHAIN_PEM_PATH LETSENCRYPT_FULLCHAIN_PEM_PATH
         	;;
         b|B) return 0 ;;
         *) 
