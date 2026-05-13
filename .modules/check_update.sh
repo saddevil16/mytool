4bb5c399ada0a72bb57ea7d82bc0cc5e00bc427e
Faizal Refendi <faizal@null.net>
2026-05-14 04:13:46 +0800

    chore: add repo url

--- .modules/check_update.sh
+++ .modules/check_update.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
 check_update() {
-	local repo_url="https://raw.githubusercontent.com/saddevil16/mytool/refs/heads/main/.modules/version.txt"
+	local repo_version_url="https://raw.githubusercontent.com/saddevil16/mytool/refs/heads/main/.modules/version.txt"
+	local repo_url="https://github.com/saddevil16/mytool.git"
 	local currentVersion
 	local latestVersion
 
 	currentVersion=$(cat "$TOOL_DIR/.modules/version.txt")
-	latestVersion=$(curl -s "$repo_url")
+	latestVersion=$(curl -s "$repo_version_url")
 
 	echo " Current version: v$currentVersion"
 	if [ "$latestVersion" != "$currentVersion" ]; then
 		echo " New version available: v$latestVersion"
+		echo ""
+		echo " Obtain latest update from $repo_url"
 	else
 		echo " No new update."
 	fi
