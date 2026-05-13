57960bf2c07bc421ae8669aaf12e1e59a58b5299
Faizal Refendi <faizal@null.net>
2026-05-14 03:41:39 +0800

    feat: add option to check for update

--- .modules/about.sh
+++ .modules/about.sh
@@ -1,13 +1,49 @@
 #!/bin/bash
 
-version="$(cat "$TOOL_DIR/.modules/version.txt")"
-clear
-echo -e "${C}===================================================${RST}"
-echo -e "${Y} MyTool - v$version${RST}"
-echo -e "${C}---------------------------------------------------${RST}"
-echo " Simple tool for my issues quick-fix."
-echo ""
-echo -e "${G} Inquiry:${RST} contact@pejal.org"
-echo -e "${C}===================================================${RST}"
-read -r -p "Press any key to continue.. "
-return
+about_UI() {
+	local version="$(cat "$TOOL_DIR/.modules/version.txt")"
+	clear
+	echo -e "${C}===================================================${RST}"
+	echo -e "${YB} MyTool - v$version${RST}"
+	echo -e "${C}---------------------------------------------------${RST}"
+	echo " Simple tool for my issues quick-fix."
+	echo ""
+	echo -e "${GB} Inquiry:${RST} contact@pejal.org"
+	echo -e "${C}===================================================${RST}"
+}
+
+about_UI_updated() {
+	source "$TOOL_DIR/.modules/check_update.sh"
+	local version="$(cat "$TOOL_DIR/.modules/version.txt")"
+	clear
+	echo -e "${C}===================================================${RST}"
+	echo -e "${Y} MyTool - v$version${RST}"
+	echo -e "${C}---------------------------------------------------${RST}"
+	echo " Simple tool for my issues quick-fix."
+	echo ""
+	check_update
+	echo ""
+	echo -e "${G} Inquiry:${RST} contact@pejal.org"
+	echo -e "${C}===================================================${RST}"
+	read -r -p "Press any key to return.."
+	return 0
+}
+
+handle_about_input() {
+
+	echo -e "${YB}Check for update?${RST} (y/n)"
+	read -r -p "or press any key to return.. " input_about
+	case "$input_about" in
+		y|Y)
+			about_UI_updated
+			;;
+		n|N|*)
+			return 0;;
+	esac
+}
+
+show_about() {
+	about_UI
+	handle_about_input
+	return 0
+}
