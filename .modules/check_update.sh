298e9b7d8a45e75d3466978735047650604fdf4c
Faizal Refendi <faizal@null.net>
2026-05-15 06:31:19 +0800

    feat: refactor check update

--- .modules/check_update.sh
+++ .modules/check_update.sh
@@ -1,22 +1,45 @@
 #!/bin/bash
 
+version_cmp() {
+    local v1="$1"
+    local v2="$2"
+
+    if [ "$v1" = "$v2" ]; then
+        echo "equal"
+        return
+    fi
+
+    if [ "$(printf '%s\n%s\n' "$v1" "$v2" | sort -V | head -n1)" = "$v1" ]; then
+        echo "less"
+    else
+        echo "greater"
+    fi
+}
+
 check_update() {
 	local repo_version_url="https://raw.githubusercontent.com/saddevil16/mytool/refs/heads/main/.modules/version.txt"
-	local repo_url="https://github.com/saddevil16/mytool.git"
 	local currentVersion
 	local latestVersion
 
 	currentVersion=$(cat "$TOOL_DIR/.modules/version.txt")
 	latestVersion=$(curl -s "$repo_version_url")
 
-	echo " Current version: v$currentVersion"
-	if [ "$latestVersion" != "$currentVersion" ]; then
-		echo " New version available: v$latestVersion"
-		echo ""
-		echo " Obtain latest update from $repo_url"
-		return 0 # update available
-	else
-		echo " No new update."
-		return 1 # no update
-	fi
+	case "$(version_cmp "$latestVersion" "$currentVersion")" in
+	    greater)
+	    	echo " Current version: v$currentVersion"
+	        echo " New version available: v$latestVersion"
+	        return 0 # update available
+	        ;;
+	    equal)
+	    	echo " Current version: v$currentVersion"
+	        echo " Already up to date."
+	        return 1 # no new update
+	        ;;
+	    less)
+	    	# Display current version as latese version since repo_version_url value is not yet updated (network delay)
+	    	echo " Current version: v$latestVersion"
+	    	echo " Already up to date."
+	        return 1 # no new update
+	        ;;
+	esac
 }
